0b93e1d44ff8cde112b5fa180f85acdfdb247132
ravikmatt <59700106+ravikmatt@users.noreply.github.com>
2023-08-30 13:20:10 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("ravikmatt is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("ravikmatt is Awesome!!!", "larry3d", "purple", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
